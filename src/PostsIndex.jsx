670e9f7189cacaefa3cd2b30db3a8897691ae22f
“lukenilsson” <luke.nilsson@gmail.com>
2023-04-21 14:11:02 -0400

    search filter

--- src/PostsIndex.jsx
+++ src/PostsIndex.jsx
@@ -1,19 +1,31 @@
 import { useState } from "react";
 
 export function PostsIndex(props) {
+  const [searchFilter, setSearchFilter] = useState("");
+
   console.log(props);
 
   return (
     <div id="posts-index">
       <h1>All posts</h1>
-      {props.posts.map((post) => (
-        <div key={post.id} className="nick-tv-shows">
-          <h2>{post.title}</h2>
-          <p>{post.body}</p>
-          <img src={post.image} alt="Woopsy" />
-          <button onClick={() => props.onShowPost(post)}>More Info</button>
-        </div>
-      ))}
+      Search filter:{" "}
+      <input
+        type="text"
+        value={searchFilter}
+        onChange={(event) => setSearchFilter(event.target.value)}
+      />
+      {props.posts
+        .filter((recipe) =>
+          recipe.title.toLowerCase().includes(searchFilter.toLowerCase())
+        )
+        .map((post) => (
+          <div key={post.id} className="nick-tv-shows">
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+            <img src={post.image} alt="Woopsy" />
+            <button onClick={() => props.onShowPost(post)}>More Info</button>
+          </div>
+        ))}
     </div>
   );
 }
